8488bcaea9157297cd22f142bcd6778d297c479c
bunkaboi <b.augustin@hotmail.com>
2024-08-04 15:21:49 +0200

    starting portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Pf1Component } from './pf-1/pf-1.component';
 import { Pf2Component } from './pf-2/pf-2.component';
 import { Pf3Component } from './pf-3/pf-3.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [Pf1Component, Pf2Component, Pf3Component],
+  imports: [CommonModule, Pf1Component, Pf2Component, Pf3Component],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
