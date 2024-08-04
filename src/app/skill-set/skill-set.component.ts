606147ce50d6a780515330e93b3467aa47eb599b
bunkaboi <b.augustin@hotmail.com>
2024-08-04 14:28:40 +0200

    skillset finished without bugfix

--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
+
+  hover = false;
 
 }
